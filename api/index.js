970e11d0cf17fbe9ebe32dfdbdd891d46e2a85f7
Dario <dasemenzato@hotmail.com>
2019-10-19 07:12:57 -0300

    aviso de mongoose solucionado

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ var app = require('./app');
 var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/mapas', (err, res) => {
+mongoose.connect('mongodb://localhost:27017/mapas', {useNewUrlParser:true},(err, res) => {
     if(err){
         throw err;
     }else{
